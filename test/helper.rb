605d29dd9ef60092b72c1046cbe0d9b666cba85c
Bryce Kerley <bkerley@brycekerley.net>
2012-05-20 15:04:35 -0400

    pull in cambridge

--- test/helper.rb
+++ test/helper.rb
@@ -2,6 +2,8 @@ require 'shoulda-context'
 require 'mocha'
 require 'minitest/autorun'
 
+require_relative File.join('..', 'lib', 'cambridge')
+
 class CambridgeCase < MiniTest::Unit::TestCase
   include Shoulda::Context::Assertions
   include Shoulda::Context::InstanceMethods
