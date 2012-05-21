d4e01bd3a3bde216c86c746fe773a99ce15601ad
Bryce Kerley <bkerley@brycekerley.net>
2012-05-21 17:26:07 -0400

    add some math and debug

--- lib/cambridge/tokens/command.rb
+++ lib/cambridge/tokens/command.rb
@@ -7,8 +7,16 @@ module Cambridge
 
       def to_ruby
         case @name
+        when 'dump'
+          "puts @stack.inspect"
         when 'prints'
           "puts @stack.pop"
+        when 'printi'
+          "puts @stack.pop.to_s"
+        when 'add'
+          "@stack.push(@stack.pop + @stack.pop)"
+        when 'mult'
+          "@stack.push(@stack.pop * @stack.pop)"
         end
       end
     end
