ce06ca4454ed748e552297e2a109b2ca2eb78936
Bryce Kerley <bkerley@brycekerley.net>
2012-05-21 17:26:15 -0400

    math tests

--- test/test_hello.rb
+++ test/test_hello.rb
@@ -12,4 +12,16 @@ class TestHello < CambridgeCase
       Cambridge.run_file @filename
     end
   end
+
+  context 'Math demo' do
+    setup do
+      @filename = File.join(File.dirname(__FILE__), 'fixtures', 'math.cambridge')
+    end
+
+    should 'print to stdout' do
+      $stdout.expects(:puts).with('44')
+
+      Cambridge.run_file @filename
+    end
+  end
 end
